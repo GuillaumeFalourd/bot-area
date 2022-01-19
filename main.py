4deee419e95730f0f8a27cae97cbc3a268c791ef
Guillaume Falourd <guillaume.falourd@zup.com.br>
2022-01-19 18:44:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ while True:
     out = request.getOutValue()
 
     lineSplit = open("log", "r").readline().split(' ')
+    
+    print("U", lineSplit[0])
+    print("P", lineSplit[1])
 
     timeToSleep = request.vote(lineSplit[0], lineSplit[1], out)
 
